fa211e352497c91c7e1f5ffc4e671994838fd4df
kmilov <kmil0cv@gmail.com>
2012-06-21 11:01:17 -0400

    First commit of the Boilerplate

---
+++ src/app.js
@@ -0,0 +1,14 @@
+/**
+ * App initial class
+ * @author kmil0cv
+ */
+App = function() {
+	
+	/**
+	 * main function,  entry point of the application
+	 * @author kmil0cv
+	 */
+	this.init = function() {
+		alert('Device Ready!');
+	}
+}
